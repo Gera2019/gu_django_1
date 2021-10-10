b19cf6145f3ee997c8d4f5eb4a28575e2f786c75
Gera2019 <gumanoid2@gmail.com>
2021-10-10 11:11:04 +0300

    lesson 2 completed

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+import json
 
 # Create your views here.
 def products(request):
-    return render(request, 'mainapp/products.html')
+    title = 'Каталог'
+    with open ('mainapp/products_list.json', 'r') as f:
+        links_menu = json.load(f)
+
+    context = {'title': title, 'links_menu': links_menu}
+    return render(request, 'mainapp/products.html', context)
