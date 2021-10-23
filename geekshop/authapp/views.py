e57cc1a6adb7aa52ff7288e5792670acfd930ea5
Gera2019 <gumanoid2@gmail.com>
2021-10-24 00:55:10 +0300

    lesson 6 completed

--- geekshop/authapp/views.py
+++ geekshop/authapp/views.py
@@ -8,7 +8,8 @@ from authapp.forms import ShopUserLoginForm, ShopUserRegisterForm, ShopUserEditF
 
 def login(request):
     title = 'вход'
-    login_form = ShopUserLoginForm(data=request.POST)
+    login_form = ShopUserLoginForm(data=request.POST or None)
+    next = request.GET['next'] if  'next' in request.GET.keys() else ''
     if request.method == 'POST' and login_form.is_valid():
         username = request.POST['username']
         password = request.POST['password']
@@ -16,11 +17,15 @@ def login(request):
         user = auth.authenticate(username=username, password=password)
         if user and user.is_active:
             auth.login(request, user)
-            return HttpResponseRedirect(reverse('main'))
+            if 'next' in request.POST.keys():
+                return HttpResponseRedirect(request.POST['next'])
+            else:
+                return HttpResponseRedirect(reverse('main'))
 
     context = {
         'title':title,
         'login_form': login_form,
+        'next': next,
     }
     return render(request, 'authapp/login.html', context)
 
@@ -34,7 +39,6 @@ def register(request):
 
     if request.method == 'POST':
         register_form = ShopUserRegisterForm(request.POST, request.FILES)
-
         if register_form.is_valid():
             register_form.save()
             return HttpResponseRedirect(reverse('auth:login'))
--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,14 +1,31 @@
 from django.shortcuts import render, get_object_or_404
 from mainapp.models import ProductsCategory, Product
 from basketapp.models import Basket
+import random
 
-# Create your views here.
+
+def get_basket(user):
+    if user.is_authenticated:
+        return Basket.objects.filter(user=user)
+    else:
+        return []
+
+def get_hot_product():
+    products = Product.objects.all()
+    return random.sample(list(products), 1)[0]
+
+def get_same_products(hot_product):
+    same_products = Product.objects.filter(category=hot_product.category).exclude(pk=hot_product.pk)[:3]
+    return same_products
+
 
 def products(request, pk=None):
     title = 'Каталог'
     links_menu = ProductsCategory.objects.all()
     products = Product.objects.order_by('price')
-    basket = Basket.objects.filter(user=request.user)
+    basket = get_basket(request.user)
+    hot_product = get_hot_product()
+    same_products = get_same_products(hot_product)
 
     if pk is not None:
         if pk == 0:
@@ -27,13 +44,28 @@ def products(request, pk=None):
         }
         return render(request, 'mainapp/products.html', context )
 
-    same_products = Product.objects.all()[1:2]
-
     context = {
         'title': title,
         'links_menu': links_menu,
         'products': products,
+        'hot_product': hot_product,
         'same_products': same_products,
         'basket': basket,
     }
     return render(request, 'mainapp/products.html', context)
+
+def product(request, pk):
+    title = 'Описание товара'
+    links_menu = ProductsCategory.objects.all()
+    product = get_object_or_404(Product, pk=pk)
+    same_products =  get_same_products(product)
+    basket = get_basket(request.user)
+
+    context = {
+        'title': title,
+        'links_menu': links_menu,
+        'product': product,
+        'same_products': same_products,
+        'basket': basket,
+    }
+    return render(request, 'mainapp/product.html', context)
